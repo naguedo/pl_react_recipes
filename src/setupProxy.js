914910955929300df55b64773e253ccc07b3ae6a
Damien <damien.brincourt@qoqa.com>
2020-10-29 17:27:13 +0100

    proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:3001',
+            target: 'https://pl-recipes.herokuapp.com',
             changeOrigin: true,
         })
     );
